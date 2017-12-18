166cc57da317d537ec18c5b04f165475f426ffdf
Philippe M. Chiasson <gozer@ectoplasm.org>
2017-12-18 12:00:06 -0500

    When installing fail2ban, make sure to keep firewalled in permissive mode
    Fixes #160 and nubisproject/nubis-base#765

--- nubis/puppet/fail2ban.pp
+++ nubis/puppet/fail2ban.pp
@@ -5,3 +5,12 @@ service {'fail2ban':
   ensure => 'stopped',
   enable => true,
 }
+
+if $osfamily == 'RedHat' {
+  exec { 'firewalld default accept':
+    command => '/usr/bin/firewall-offline-cmd --zone=public --set-target=ACCEPT',
+    require => [
+      Package['fail2ban'],
+    ],
+  }
+}
